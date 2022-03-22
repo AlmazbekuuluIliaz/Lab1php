f487cdbe93ea7c32e2bda3a94dfcb2feb784196e
Iliaz <almazbek-03@mail.ru>
2022-03-23 00:53:08 +0300

    origin

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab1php</title>
+</head>
+<body>
+    <header class="Header">
+        <div class="Container">
+            <p class="main__text">Лабораторная работа №1</p>
+        </div>
+    </header>
+    <main class="Main">
+        <div class="Container">
+            <p class="main__text">
+                <?php echo "Hello, World!";?>
+            </p>
+        </div>
+    </main>
+    <footer class="Footer">
+        <div  class="Container">
+            <p class="footer__text">Задание для самостоятельной работы!</p>
+        </div>
+    </footer>
+</body>
+</html>
